cf5550e831069d344520d004ae42f928bfc1e235
moda <moda2047@naver.com>
2024-02-13 13:30:16 +0900

    리액트 배열 활용

--- week04/day01proj01/src/App.js
+++ week04/day01proj01/src/App.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 function App() {
+  const [fruits, setFruits] = useState([]);
+  const [inputVal, setInputVal] = useState("");
   return (
     <>
       <h1>Hello world</h1>
+      <div>
+        <input
+          onChange={(e) => {
+            setInputVal(e.target.value);
+          }}
+          type="text"
+          value={inputVal}
+        />
+        <button
+          onClick={function (e) {
+            setFruits([...fruits, inputVal]);
+          }}
+        >
+          추가하기
+        </button>
+      </div>
+      <div>
+        <ul>
+          {fruits.map(function (value, index) {
+            return <li key={index}>{value}</li>;
+          })}
+        </ul>
+      </div>
     </>
   );
 }
